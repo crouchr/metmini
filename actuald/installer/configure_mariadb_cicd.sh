7c6282819f92a807401f569bd64780f848d7882c
crouchr <richard.crouch@vonage.com>
2020-12-21 16:20:52 +0000

    Added API tests

---
+++ actuald/installer/configure_mariadb_cicd.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+# script to create the metmini database and add tables
+
+# This is version used on mrdell (Production)
+# FIXME - the ordering in here needs fixing before this script can be use in an automation pipeline
+
+mysql -u root <<MYSQL_SCRIPT
+CREATE USER 'metmini'@mrdell.localdomain identified by 'metmini';
+CREATE USER 'grafanaReader' IDENTIFIED BY 'grafanasecret';
+CREATE DATABASE metminidb;
+GRANT SELECT ON metminidb.actual TO 'grafanaReader';
+GRANT ALL ON metminidb.* to 'metmini' identified by 'metmini';
+FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'metmini'@mrdell.localdomain;
+MYSQL_SCRIPT
+
+mysql -u root <<MYSQL_SCRIPT
+USE metminidb;
+CREATE TABLE metminilogs
+(
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ts_local DATETIME NOT NULL,
+ts_utc DATETIME NOT NULL,
+julian INT NOT NULL,
+pressure INT NOT NULL,
+ptrend VARCHAR(10) NOT NULL,
+wind_quadrant VARCHAR(10) NOT NULL,
+wind_strength VARCHAR(10) NOT NULL,
+forecast VARCHAR(128) NOT NULL,
+bforecast VARCHAR(32) NOT NULL,
+oforecast VARCHAR(32) NOT NULL,
+coverage VARCHAR(16) NOT NULL,
+location VARCHAR(64) NOT NULL,
+yest_rain int NOT NULL,
+yest_wind VARCHAR(10) NOT NULL,
+yest_min_temp INT NOT NULL,
+yest_max_temp INT NOT NULL,
+data_type VARCHAR(16) NOT NULL
+);
+MYSQL_SCRIPT
+
+mysql -u root <<MYSQL_SCRIPT
+USE metminidb;
+CREATE TABLE actual
+(
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ts_local DATETIME NOT NULL,
+ts_utc DATETIME NOT NULL,
+julian INT NOT NULL,
+hour_utc INT NOT NULL,
+location VARCHAR(64) NOT NULL,
+main VARCHAR(32) NOT NULL,
+description VARCHAR(32) NOT NULL,
+pressure INT NOT NULL,
+wind_speed FLOAT NOT NULL,
+wind_deg INT NOT NULL,
+wind_quadrant VARCHAR(8) NOT NULL,
+wind_strength INT NOT NULL,
+wind_gust FLOAT NOT NULL,
+temp FLOAT NOT NULL,
+feels_like FLOAT NOT NULL,
+dew_point FLOAT NOT NULL,
+uvi FLOAT NOT NULL,
+humidity INT NOT NULL,
+visibility INT NOT NULL,
+rain FLOAT NOT NULL,
+snow FLOAT NOT NULL,
+coverage INT NOT NULL,
+source VARCHAR(32) NOT NULL,
+lat VARCHAR(8) NOT NULL,
+lon VARCHAR(8) NOT NULL,
+tz VARCHAR(32) NOT NULL,
+tz_offset INT NOT NULL,
+ts_epoch INT NOT NULL,
+sunrise_local TIMESTAMP NOT NULL,
+sunset_local TIMESTAMP NOT NULL
+);
+MYSQL_SCRIPT
+
+mysql -u root <<MYSQL_SCRIPT
+USE metminidb;
+CREATE TABLE forecasts
+(
+id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+ts_local DATETIME NOT NULL,
+ts_utc DATETIME NOT NULL,
+julian INT NOT NULL,
+location VARCHAR(64) NOT NULL,
+pressure INT NOT NULL,
+ptrend VARCHAR(10) NOT NULL,
+wind_quadrant VARCHAR(8) NOT NULL,
+wind_strength INT NOT NULL,
+slope FLOAT NOT NULL,
+source VARCHAR(32) NOT NULL,
+forecast VARCHAR(128) NOT NULL
+);
+MYSQL_SCRIPT
---
+++ actuald/installer/install_packages.sh
@@ -0,0 +1,2 @@
+# installl any packages needed to make centos7-docker VBOX into our application server
+sudo yum install -y mariadb-server
